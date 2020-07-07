881f3f91d4936c285b21ba53c5f6a081d8bb2259
Gustavo Huarcaya <diavolo@gahd.net>
2020-07-07 14:38:49 -0500

    update check --deploy

--- gahd/settings/prod.py
+++ gahd/settings/prod.py
@@ -7,4 +7,25 @@ ALLOWED_HOSTS += [
     'gahd.net'
 ]
 
+# https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/#csrf-cookie-secure
+CSRF_COOKIE_SECURE = True
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#secure-hsts-include-subdomains
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#secure-hsts-preload
+SECURE_HSTS_PRELOAD = True
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#secure-hsts-seconds
+SECURE_HSTS_SECONDS = 3600
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#secure-referrer-policy
+SECURE_REFERRER_POLICY = 'same-origin'
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#secure-ssl-redirect
+SECURE_SSL_REDIRECT = True
+
+# https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/#session-cookie-secure
+SESSION_COOKIE_SECURE = True
+
 MEDIA_ROOT = str(DATA_DIR.joinpath('media'))
