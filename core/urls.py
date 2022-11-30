8c20ba0e274257df0b7bb8e21a7763deae1b5a47
Gustavo Huarcaya <diavolo@gahd.net>
2022-11-30 15:49:42 -0500

    chore: update core urls name

--- core/urls.py
+++ core/urls.py
@@ -8,28 +8,39 @@ from . import views, feeds
 app_name = 'core'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('favicon.ico', RedirectView.as_view(url='static/img/favicon.ico',
-                                             permanent=True),
+
+    # REDIRECTS
+
+    path('favicon.ico',
+         RedirectView.as_view(url='static/img/favicon.ico', permanent=True),
          name='favicon'),
-    path('humans.txt', TemplateView.as_view(template_name="core/humans.txt",
-                                            content_type='text/plain'),
+    path('index.xml', RedirectView.as_view(url='/feed/'), name='go-to-feed'),
+
+    # OTHER SECTIONS WITH CUSTOM URLS
+
+    path('humans.txt',
+         TemplateView.as_view(template_name="core/humans.txt",
+                              content_type='text/plain'),
          name='humans'),
     path('articles/', views.ArticleListView.as_view(), name='article-list'),
-    path('index.xml', RedirectView.as_view(url='/feed/'), name='go-to-feed'),
     path('feed/', feeds.LatestEntriesFeed(), name='feed'),
     path('search/', views.SearchView.as_view(), name='search'),
+
+    # TAGS
+
     path('tags/', views.TagListView.as_view(), name='tag-list'),
-    # articles by tag
+    # posts (articles, pages, and notes) by tag
     path('tags/<slug:tag_slug>/', views.PostsByTagListView.as_view(),
-         name='article-list-by-tag'),
-    # articles by tag feed
+         name='post-list-by-tag'),
+    # posts by tag feed
     path('tags/<slug:tag_slug>/feed/', feeds.EntriesByTagFeed(),
-         name='article-list-by-tag-feed'),
+         name='post-list-by-tag-feed'),
+
+    # PAGES AND ARTICLES
+
     # page detail or articles by category
     path('<slug:page_slug>/', views.PageOrCategoryView.as_view(),
-         name='page-detail'),
-    path('<slug:page_slug>/', views.PageOrCategoryView.as_view(),
-         name='article-list-by-category'),
+         name='page-detail-or-article-list-by-category'),
     # articles by category feed
     path('<slug:category_slug>/feed/', feeds.EntriesByCategoryFeed(),
          name='article-list-by-category-feed'),
