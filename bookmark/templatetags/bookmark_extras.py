22ccd9311ea8cdf706d8c69886934085d9624fb3
Gustavo Huarcaya <diavolo@gahd.net>
2023-09-14 21:12:55 -0500

    feat: use Django custom template tags to show bookmarks netloc

---
+++ bookmark/templatetags/bookmark_extras.py
@@ -0,0 +1,24 @@
+# django.template.Library.filter()
+from urllib.parse import urlparse
+from django import template
+
+register = template.Library()
+
+
+def bookmark_domain(value):
+    """Bookmark custom template to show the network location of the saved URI.
+
+    - https://docs.djangoproject.com/en/4.2/howto/custom-template-tags/
+    - https://docs.python.org/3/library/urllib.parse.html
+
+    Args:
+        value (str): Bookmark's URI
+
+    Returns:
+        str: Network Location.
+    """
+    bookmark = urlparse(value)
+    return f"{bookmark.netloc}"
+
+
+register.filter("bookmark_domain", bookmark_domain)
