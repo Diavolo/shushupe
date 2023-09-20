6e2cedbbe2698dd6ebdfcc4af1f14a1d5b30055a
Gustavo Huarcaya <diavolo@gahd.net>
2023-09-19 20:25:37 -0500

    feat: add truncate_html function

--- core/utils/post.py
+++ core/utils/post.py
@@ -35,3 +35,38 @@ def get_full_path(item_url: str) -> str:
         str: Full path of the item (including the base URL i.e. https://...)
     """
     return f"{_site_url}{item_url}"
+
+
+def truncate_html(html_content, max_length):
+    # Parse the HTML content
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Initialize the truncated content
+    truncated_content = []
+
+    # Initialize a variable to keep track of the current content length
+    current_length = 0
+
+    # Iterate through the HTML elements
+    for element in soup.recursiveChildGenerator():
+        # If the element is a string (text), calculate its length and add it to the truncated content
+        if isinstance(element, str):
+            text = str(element)
+            current_length += len(text)
+
+            # If the current length exceeds the maximum length, truncate the text
+            if current_length > max_length:
+                # remaining_length = max_length - (current_length - len(text))
+                # truncated_text = text[:remaining_length]
+                # truncated_content.append(truncated_text)
+                break
+            else:
+                truncated_content.append(text)
+        else:
+            # For non-text elements (tags), add them to the truncated content
+            truncated_content.append(str(element))
+
+    # Join the truncated content to form the final HTML
+    truncated_html = ''.join(truncated_content)
+
+    return truncated_html
