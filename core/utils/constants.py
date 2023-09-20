224481f6a5940c4cfbd2b9194333a9a14c8703e2
Gustavo Huarcaya <diavolo@gahd.net>
2023-09-20 11:58:44 -0500

    feat: add constants and config utils file

---
+++ core/utils/constants.py
@@ -0,0 +1,2 @@
+PASSWD_PROTECTED_MSG = 'Post protected with a password. ' + \
+    'If you know the password, enter the site and write it.'
---
+++ core/utils/shushupe.py
@@ -0,0 +1,5 @@
+from django.conf import settings
+
+
+SITE_NAME = settings.SITE_NAME
+SITE_URL = settings.SITE_URL
